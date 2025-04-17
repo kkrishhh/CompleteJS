027af7b40c61f1c98c702c10634ed2c5f4bed68b
kkrishhh <soham6218@gmail.com>
2025-04-17 18:37:49 +0530

    data types

---
+++ Part-1/datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //Treat all Js code as newer version 
+
+let number  = 19490;
+let Fname = 'soham';
+let married = false;
+let temp = null;
+let stocks;
+
+console.log(typeof stocks);
+console.log(typeof married);
+console.log(typeof temp);
+console.log(typeof number);
+console.log(typeof Fname);
