2f0565b83155a711fef01b761052115d4b229e33
kkrishhh <soham6218@gmail.com>
2025-04-19 21:56:18 +0530

    conversion of data types

--- Part-1/02_conversion_operations.js
+++ Part-1/02_conversion_operations.js
@@ -5,3 +5,13 @@ console.log(typeof score);
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+let value = 1;
+console.log(typeof value);
+console.log(value);
+
+let valueInBoolean = Boolean(value);
+console.log(typeof valueInBoolean);
+console.log(valueInBoolean);
+
+
