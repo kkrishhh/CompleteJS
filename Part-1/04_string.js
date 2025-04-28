4185dfb5d44ae2fa4ec0e425f614c0aaab2c2c45
kkrishhh <soham6218@gmail.com>
2025-04-29 01:41:33 +0530

    learned more string functions

--- Part-1/04_string.js
+++ Part-1/04_string.js
@@ -3,3 +3,10 @@ let value = 'programmer';
 let num = 5;
 
 console.log(`hey guys my name is ${name} and i'm a skilled ${value} and i will get ${num} million $ of compensation`);
+
+//another way of doing it 
+
+const gameName = new String ('sohampawar');
+console.log(gameName);
+console.log(gameName.length);
+console.log(gameName.indexOf('a'));
