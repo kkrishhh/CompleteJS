75db5be1625b1908c35902eb1523713de389a3be
kkrishhh <soham6218@gmail.com>
2025-05-10 23:32:03 +0530

    crazy date function

---
+++ Part-1/06_date.js
@@ -0,0 +1,7 @@
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toTimeString());
+console.log(myDate.toDateString());
+console.log(myDate.toUTCString());
+console.log(myDate);
