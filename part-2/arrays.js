193a7808d3362314b3616a8fa5f1895d24234fbb
kkrishhh <soham6218@gmail.com>
2025-05-11 00:13:01 +0530

    arrays method

--- part-2/arrays.js
+++ part-2/arrays.js
@@ -1,3 +1,14 @@
 const myarr = [0,1,2,3,4,5,6];
 const myarr2 = ["locae","shaha","zubui"];
 console.log(myarr2[2]);
+
+myarr.push(7);
+console.log(myarr);
+
+//myarr.pop();
+//console.log(myarr2);
+
+myarr.unshift(22);
+console.log(myarr);
+myarr.shift();
+console.log(myarr);
