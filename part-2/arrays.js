243892f132a4a0d8f12299189519a140fdf9d679
kkrishhh <soham6218@gmail.com>
2025-05-11 16:01:36 +0530

    concat

--- part-2/arrays.js
+++ part-2/arrays.js
@@ -5,15 +5,19 @@ const myarr2 = ["locae","shaha","zubui"];
 myarr.push(7);
 //console.log(myarr);
 
-//myarr.pop();
+myarr.pop();
 //console.log(myarr2);
 
-/*myarr.unshift(22);
+myarr.unshift(22);
 console.log(myarr);
 myarr.shift();
-console.log(myarr);*/
+console.log(myarr);
 
 //slice spice
 
 console.log(myarr.slice(1,3));
 console.log(myarr.splice(2,5));
+
+const superstar = ['soham, laddo, jalebi, xijin, messiah']
+console.log(superstar.concat(myarr));
+
