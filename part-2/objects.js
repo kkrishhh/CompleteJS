698a0ae08d0a80a622b8c67d8cc59d3825ab0162
kkrishhh <soham6218@gmail.com>
2025-05-11 16:22:38 +0530

    objects

---
+++ part-2/objects.js
@@ -0,0 +1,16 @@
+//sinleton 
+//  object literal
+let users = {
+    name : 'soham',
+    weight: '60',
+    email: "soh@gmail.com",
+    isloggedin: true,
+
+};
+
+console.log(users.name);
+console.log(users.weight);
+console.log(users.email);
+console.log(users.isloggedin);
+
+//  object constructor);
