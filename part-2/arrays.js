cb5e8c7b4959dedd8a867ac6ad1c3546ab1c499e
kkrishhh <168463881+kkrishhh@users.noreply.github.com>
2025-07-26 07:59:56 +0000

    splice changes the original array

--- part-2/arrays.js
+++ part-2/arrays.js
@@ -1,24 +1,27 @@
-const myarr = [0,1,2,3,4,5,6];
-const myarr2 = ["locae","shaha","zubui"];
-//console.log(myarr2[2]);
+ const myarr = [0,1,2,3,4,5,6];
+ const myarr2 = ["locae","shaha","zubui"];
+console.log(myarr2[2]);
 
-myarr.push(7);
-//console.log(myarr);
-
-myarr.pop();
-//console.log(myarr2);
-
-myarr.unshift(22);
+ myarr.push(7);
 console.log(myarr);
-myarr.shift();
-console.log(myarr);
+ myarr.pop();
+console.log(myarr2);
+
+ myarr.unshift(22);
+ console.log(myarr);
+ myarr.shift();
+ console.log(myarr);
 
 //slice spice
 
 console.log(myarr.slice(1,3));
-console.log(myarr.splice(2,5));
+ console.log(myarr.splice(2,5));
 
-const superstar = ['soham, laddo, jalebi, xijin, messiah']
-console.log(superstar.concat(myarr));
+ const superstar = ['soham, laddo, jalebi, xijin, messiah']
+ console.log(superstar.concat(myarr));
 
-console.log(myarr.includes(22));
+ console.log(myarr.includes(22));
+let superhero = ['soham', 'steve', 'tony', 'bruce', 'clark'];
+console.log(superhero.slice(1, 3));
+console.log(superhero.splice(2, 4));//this modifies the original array
+console.log(superhero);
