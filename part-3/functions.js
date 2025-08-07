ce2f16e10ece9a77e0c086d1db7598ed11c91e7e
kkrishhh <168463881+kkrishhh@users.noreply.github.com>
2025-08-07 13:41:22 +0000

    fns

---
+++ part-3/functions.js
@@ -0,0 +1,15 @@
+function sum(a, b){
+    const sumValue = a + b;
+    return sumValue;
+}
+
+const value = sum( 5, 9);
+console.log(value)
+
+function mul( a, b){
+    const mulValues = a * b;
+    return mulValues;
+}
+
+let multiply = mul( 4, 5);
+console.log(multiply);
